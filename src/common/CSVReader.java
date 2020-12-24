e4449943e537f9187080dc67753dc265023a325f
Luis Suazo <luisrsuazo@gmail.com>
2020-12-23 18:19:15 -0600

    Functioning on csv reader.

--- src/common/CSVReader.java
+++ src/common/CSVReader.java
@@ -4,9 +4,11 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class CSVReader {
+
     static ArrayList<String[]> readCSV(File csv_file) {
-        ArrayList<String[]> fields = new ArrayList<String[]>();
+        ArrayList<String[]> fields = new ArrayList<>();
         BufferedReader freader = null;
+
         try {
             freader = new BufferedReader(new FileReader(csv_file));
         } catch (FileNotFoundException e) {
@@ -14,15 +16,29 @@ public class CSVReader {
         }
 
         String currentLine = null;
-        while (true) {
-            try {
-                if (!((currentLine = freader.readLine()) != null)) break;
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+            while((currentLine = freader.readLine()) != null) {
+                fields.add(currentLine.split(","));
             }
-            System.out.println(currentLine);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return null;
+        return fields;
+    }
+
+
+    public static void main(String[] args) {
+        String dir = System.getProperty("user.dir");
+        System.out.println(dir);
+        File csv = new File("resources/simple_csv.csv");
+        ArrayList<String[]> result = CSVReader.readCSV(csv);
+        for(String[] strings : result) {
+            for(String string : strings) {
+                System.out.print(string + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
