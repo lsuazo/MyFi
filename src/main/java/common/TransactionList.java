6a8e823fc0b0b7b62c0524f60aa3ae2b3936cdf0
Luis Suazo <luisrsuazo@gmail.com>
2021-01-08 13:33:53 -0600

    TransactionList cant be iterated.

--- src/main/java/common/TransactionList.java
+++ src/main/java/common/TransactionList.java
@@ -1,9 +1,10 @@
 package main.java.common;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 
-public class TransactionList{
+public class TransactionList implements Iterable<Transaction>{
     final public static Map<Integer, Transaction.Fields> fieldNames = Map.of(
             0, Transaction.Fields.DATE,
             1, Transaction.Fields.AMOUNT,
@@ -18,4 +19,20 @@ public class TransactionList{
     public Transaction getTransaction(int number) { return this.transactions.get(number);}
     public void addTransaction(Transaction transaction) { transactions.add(transaction); }
     public int length(){ return transactions.size(); }
+
+    public Iterator<Transaction> iterator() { return  new TransactionsIterator(); }
+
+    class TransactionsIterator implements Iterator<Transaction> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < length();
+        }
+
+        @Override
+        public Transaction next() {
+            return getTransaction(index++);
+        }
+    }
 }
